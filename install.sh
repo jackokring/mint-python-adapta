0b48e69e0848c97265b2e2ee43779e582ba1a2f4
jackokring <jackokring@gmail.com>
2025-09-09 09:35:01 +0000

    better install with some options

--- install.sh
+++ install.sh
@@ -7,6 +7,16 @@ cd "$(realpath "$(dirname "$0")")" || exit 1
 VER=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))')
 NAME=$(sed -nr "s/^name = \"(.*)\"$/\1/p" <pyproject.toml)
 
+echo "Making or replacing a virtual environment in '$CWD'"
+echo "Python version '$VER'"
+echo "Project name '$NAME'"
+echo
+echo "You can now abort the install if you need to prepare."
+
+if ./yes-no.sh "abort"; then
+	exit 0
+fi
+
 # ivalidate older
 rm -rf bin include lib "$NAME/applications" "$NAME/icons" "$NAME/locale"
 echo "Clean ..."
@@ -18,21 +28,40 @@ echo "VENV ..."
 # set virtual environment
 . ./bin/activate
 
-# add libadapta-1 and gum TUI tool for nicer scripts
-sudo dpkg -i ./*.deb
+echo "The next bit depends on Linux Mint being installed."
+echo "If you have Xia you can install libAdapta (not needed if you have Zara or later)."
+echo "This has not been tested on Wilma or earlier."
 
-# apt installs might be necessary
-sudo apt install -y libadwaita-* libadwaita-*-dev gir1.2-adw-* python3-gi
+if ./yes-no.sh "install libAdapata"; then
+	# add libadapta-1 and gum TUI tool for nicer scripts
+	sudo dpkg -i ./*.deb
+fi
 
-# international
-sudo apt install gettext*
+echo "If your disto Ubuntu LTS based (Noble or later), you can install the necessary packages."
+echo "It might work on other versions or even debian 12 or 13, but has not been tested."
+
+if ./yes-no.sh "install gtk4 instrospection and pyhton libs"; then
+	# apt installs might be necessary
+	sudo apt install -y libadwaita-* libadwaita-*-dev gir1.2-adw-* python3-gi
+fi
+
+echo "If you wish to develop for other locales, the following might be required."
+
+if ./yes-no.sh "gettext language locale utilities"; then
+	# international
+	sudo apt install gettext*
+fi
 
 # install pips
 ./bin/pip install -r requirements.txt
 
+echo "An error about 'Adap.pyi' indicates a fall back to libAdwaita."
+echo "The symbols for libAdapta might not have installed."
 # make ADap symbol visible
 # change this when the version bumps from 3.12
 cp Adap.pyi "lib/python$VER/site-packages/gi-stubs/repository/Adap.pyi"
+
+echo "Perfoming first build."
 
 # build once
 ./build.sh
