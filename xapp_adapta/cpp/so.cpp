8f18cd2ce34c9dd7ffe44130e3a990ebfc716249
jackokring <jackokring@gmail.com>
2025-09-24 06:52:03 +0000

    lua loader

--- xapp_adapta/cpp/so.cpp
+++ xapp_adapta/cpp/so.cpp
@@ -1,10 +1,23 @@
 #include "so.hpp"
 #include <libintl.h>
 // python version replaced by./build.sh script
+#include <lua5.1/lauxlib.h>
+#include <lua5.1/lua.h>
 #include <lua5.1/lua.hpp>
+#include <lua5.1/lualib.h>
 #include <python3.12/Python.h>
 
+// main lua instance state
+lua_State *L;
+
 PyObject *hello(PyObject *, PyObject *) {
+  L = luaL_newstate();
+  luaL_openlibs(L);
+  // load init.lua file
+  if (luaL_dostring(L, "require(\"init\")")) {
+    // error
+    printf("%s", lua_tostring(L, -1));
+  }
   return PyUnicode_FromString(_("C++ module loaded"));
 }
 
