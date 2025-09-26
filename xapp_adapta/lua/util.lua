9311c9ce6611019f754e466842e6ebd79083d421
jackokring <jackokring@gmail.com>
2025-09-26 00:25:55 +0000

    util

--- xapp_adapta/lua/util.lua
+++ xapp_adapta/lua/util.lua
@@ -23,29 +23,26 @@ _G.script_path = function()
 	if is_win() then
 		str = str:sub(2):gsub("/", "\\")
 	end
-	return str:match("(.*" .. path_separator() .. ")")
+	return str:match("(.*\\" .. path_separator() .. ")") or ("." .. path_separator())
 end
 
 -- os utilities not _G ones
 -- this assignment works, some kind of module local "os", and not "_G.os"
 os = novaride.track(os)
 
----replace double quotes by escaped double quotes only
----then escape $ and add surrounding double quotes
 ---useful for escaping shell arguments for os.execute()
 ---@param chars string
 ---@return string
 os.shell_quote = function(chars)
-	-- and then there's $ as in os $HOME etc.
-	local q = string.gsub(string.gsub(chars, "\\", "\\\\"), '"', '\\"')
-	return '"' .. string.gsub(q, "[^\\]%$", "\\$") .. '"'
+	--use single quote behaviour
+	return " '" .. string.gsub(chars, "'", "'\\''") .. "' "
 end
 
 ---check if a command exists
 ---@param cmd string
 ---@return boolean
 os.has = function(cmd)
-	return os.execute() == true and os.execute("which " .. cmd) == true
+	return not is_win() and os.execute("which " .. cmd) == true
 end
 
 os = novaride.untrack(os)
